a27b82fe40b3ac9d7ce9b2cc0c4f4c3e1bb6aea0
AykhaneB <AykhaneB@TITAN08>
2023-05-15 20:02:57 +0400

    Add project files.

---
+++ FrontToBackTask/Program.cs
@@ -0,0 +1,27 @@
+
+namespace FrontToBackTask
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddMvc();
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=home}/{action=index}/{id?}");
+            });
+
+            app.Run();
+
+        }
+    }
+}
